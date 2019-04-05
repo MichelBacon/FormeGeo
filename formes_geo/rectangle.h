1181279c8f4e05a0cb27fa389a5cb10ff293063c
Louca Létourneau <1733830@cegepba.qc.ca>
2019-04-05 11:41:10 -0400

    debug

---
+++ formes_geo/rectangle.h
@@ -0,0 +1,23 @@
+#ifndef rectangleH
+#define rectangleH
+#include "figure.h"
+
+class FormeRectangle:public Figure
+{
+  protected:
+    int largeur;
+	int hauteur;
+  public:
+	FormeRectangle();
+	FormeRectangle(int x, int y, int largeu, int hauteur);
+	FormeRectangle(const FormeRectangle&);
+	~FormeRectangle();
+	void setLargeur(int);
+	void setHauteur(int);
+	int getLargeur() const;
+	int getHauteur() const;
+	virtual float calculerAire();
+	virtual float calculerperimetre();
+	virtual void selectionner(int,int);
+};
+#endif
